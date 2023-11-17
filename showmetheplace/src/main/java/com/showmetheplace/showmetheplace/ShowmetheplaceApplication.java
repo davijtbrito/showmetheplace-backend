644a5b8f49eee5526af36740166efcbbd1ba9263
davibrito1218 <davibrito1218@gmail.com>
2023-11-17 13:36:32 +0000

    initial project commit

---
+++ showmetheplace/src/main/java/com/showmetheplace/showmetheplace/ShowmetheplaceApplication.java
@@ -0,0 +1,13 @@
+package com.showmetheplace.showmetheplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowmetheplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowmetheplaceApplication.class, args);
+	}
+
+}
---
+++ showmetheplace/src/test/java/com/showmetheplace/showmetheplace/ShowmetheplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.showmetheplace.showmetheplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowmetheplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
