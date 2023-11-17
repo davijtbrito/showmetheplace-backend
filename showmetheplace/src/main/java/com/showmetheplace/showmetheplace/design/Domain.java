f8d56ead0c99be2f27bffda56e6604df1dc18aaf
davibrito1218 <davibrito1218@gmail.com>
2023-11-17 14:34:09 +0000

    defining design

---
+++ showmetheplace/src/main/java/com/showmetheplace/showmetheplace/design/Domain.java
@@ -0,0 +1,9 @@
+package com.showmetheplace.showmetheplace.design;
+
+/**
+ * the Domain corresponds to tables in the database. 
+ */
+public interface Domain {
+
+    Long getId();
+}
---
+++ showmetheplace/src/main/java/com/showmetheplace/showmetheplace/design/DomainEvent.java
@@ -0,0 +1,9 @@
+package com.showmetheplace.showmetheplace.design;
+
+/**
+ * Define domain events as Java classes that represent significant state changes or events within the domain.
+ * You can use these classes to communicate changes or trigger additional actions. 
+ */
+public interface DomainEvent {
+    
+}
